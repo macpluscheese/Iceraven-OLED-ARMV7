bcb4ce2964e23bff13f0ea98321cf4a6f0eda4ee
Goodluck Okereke <32042715+GoodyOG@users.noreply.github.com>
2024-11-30 11:47:18 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-data=$(curl -s https://github.com/fork-maintainers/iceraven-browser/releases/latest | grep -o 'tag/iceraven-[^"]*' | head -n 1 | cut -d/ -f2 | sed 's/iceraven-//')
+set -e
 
-# Construct the URL for the IceRaven APK
-apk="https://github.com/fork-maintainers/iceraven-browser/releases/download/iceraven-$data/iceraven-$data-browser-arm64-v8a-forkRelease.apk"
+# Decompile APK
+apktool d iceraven.apk -o iceraven-decompiled
 
-wget -q $apk -O latest.apk
+# Navigate to the res/values-night directory and edit colors.xml
+cd iceraven-decompiled/res/values-night
+sed -i 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">@color\/photonBlack<\/color>/g' colors.xml
+sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' colors.xml
+cd ../../..
 
-wget -q https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.8.1.jar -O apktool.jar
-wget -q https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool
-chmod +x apktool*
+# Recompile the APK
+apktool b iceraven-decompiled -o iceraven-modified.apk
 
-rm -rf patched patched_signed.apk
-./apktool d -s latest.apk -o patched
-rm -rf patched/META-INF
+# Create a temporary directory to store the keystore file
+mkdir keystore
+echo "$KEYSTORE_FILE" | base64 --decode > keystore/my-release-key.jks
 
-sed -i 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">@color\/photonBlack<\/color>/g' patched/res/values-night/colors.xml
-sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' patched/res/values-night/colors.xml
+# Sign the APK
+apksigner sign --ks keystore/my-release-key.jks --ks-key-alias "$KEYSTORE_ALIAS" --ks-pass env:KEYSTORE_PASSWORD --out iceraven-signed.apk iceraven-modified.apk
 
-./apktool b patched -o patched.apk --use-aapt2
-
-zipalign 4 patched.apk patched_signed.apk
-rm -rf patched patched.apk
+# Clean up
+rm -rf keystore
