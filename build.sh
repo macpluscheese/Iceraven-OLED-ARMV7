89aa162ac5b392c72e141fee85bb1ab75750eb21
Goodluck Okereke <32042715+GoodyOG@users.noreply.github.com>
2025-01-25 12:04:58 +0000

    build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 set -e
-# Explicitly use apktool.jar
-wget -q https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.8.1.jar -O apktool.jar
+# Download latest Apktool
+APKTOOL_URL=$(curl -s https://api.github.com/repos/iBotPeaches/apktool/releases/latest | grep -o "https://.*apktool_[0-9.]*\.jar")
+wget -q "$APKTOOL_URL" -O apktool.jar
+
 java -jar apktool.jar d -s iceraven.apk -o iceraven-patched
 
-# Color patching
+# Find correct smali path
+SMALI_PATH=$(find iceraven-patched -name "PhotonColors.smali")
+
+# Color patching with dynamic path
 sed -i 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">@color\/photonBlack<\/color>/g' iceraven-patched/res/values*/colors.xml
 sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' iceraven-patched/res/values*/colors.xml
 
-# Smali color modifications for true black OLED theme
-sed -i 's/ff2b2a33/ff000000/g' iceraven-patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff42414d/ff15141a/g' iceraven-patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff52525e/ff15141a/g' iceraven-patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
+# Smali color modifications
+if [ -n "$SMALI_PATH" ]; then
+  sed -i 's/ff2b2a33/ff000000/g' "$SMALI_PATH"
+  sed -i 's/ff42414d/ff15141a/g' "$SMALI_PATH"
+  sed -i 's/ff52525e/ff15141a/g' "$SMALI_PATH"
+fi
 
 java -jar apktool.jar b iceraven-patched -o iceraven-patched.apk --use-aapt2
 zipalign 4 iceraven-patched.apk iceraven-patched-signed.apk
