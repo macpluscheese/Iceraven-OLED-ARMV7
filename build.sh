38c5443541cdc4200be9c583185f4987bcc6b96d
Goodluck Okereke <32042715+GoodyOG@users.noreply.github.com>
2025-01-25 12:26:18 +0000

    build.sh

--- build.sh
+++ build.sh
@@ -6,19 +6,20 @@ wget -q "$APKTOOL_URL" -O apktool.jar
 
 java -jar apktool.jar d -s iceraven.apk -o iceraven-patched
 
-# Find correct smali path
-SMALI_PATH=$(find iceraven-patched -name "PhotonColors.smali")
-
-# Color patching with dynamic path
+# Color patching
 sed -i 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">@color\/photonBlack<\/color>/g' iceraven-patched/res/values*/colors.xml
 sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' iceraven-patched/res/values*/colors.xml
 
 # Smali color modifications
-if [ -n "$SMALI_PATH" ]; then
-  sed -i 's/ff2b2a33/ff000000/g' "$SMALI_PATH"
-  sed -i 's/ff42414d/ff15141a/g' "$SMALI_PATH"
-  sed -i 's/ff52525e/ff15141a/g' "$SMALI_PATH"
-fi
+SMALI_DIRS=($(find iceraven-patched -type d -name "smali_classes*"))
+for dir in "${SMALI_DIRS[@]}"; do
+ PHOTON_COLOR_FILE="$dir/mozilla/components/ui/colors/PhotonColors.smali"
+ if [ -f "$PHOTON_COLOR_FILE" ]; then
+   sed -i 's/ff2b2a33/ff000000/g' "$PHOTON_COLOR_FILE"
+   sed -i 's/ff42414d/ff15141a/g' "$PHOTON_COLOR_FILE"
+   sed -i 's/ff52525e/ff15141a/g' "$PHOTON_COLOR_FILE"
+ fi
+done
 
 java -jar apktool.jar b iceraven-patched -o iceraven-patched.apk --use-aapt2
 zipalign 4 iceraven-patched.apk iceraven-patched-signed.apk
