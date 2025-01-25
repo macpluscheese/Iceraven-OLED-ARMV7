fdaaf205de7b181b70610074adba71982d355349
Goodluck Okereke <32042715+GoodyOG@users.noreply.github.com>
2025-01-25 17:04:11 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Download latest Apktool
-wget -q https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.11.0.jar -O apktool.jar
+wget -q https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.11.0.jar -O apktool.jar  # Download latest version
 wget -q https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool
 chmod +x apktool*
 
@@ -16,10 +16,6 @@ rm -rf iceraven-patched/META-INF
 # Color patching 
 sed -i 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">@color\/photonBlack<\/color>/g' iceraven-patched/res/values*/colors.xml
 sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' iceraven-patched/res/values*/colors.xml
-
-# Addressing Status Bar Padding (Potential Solution)
-# Add the following line (adjust the value if needed)
-sed -i 's/android:statusBarColor="?@android:color\/transparent"?/android:statusBarColor="@color\/black"/g' iceraven-patched/res/values*/themes.xml
 
 # Recompile the APK
 ./apktool b iceraven-patched -o iceraven-patched.apk --use-aapt2
