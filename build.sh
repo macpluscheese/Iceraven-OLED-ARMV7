25c8fa180d3aaf0870603e02441a5a379d12aeb9
Goodluck Okereke <32042715+GoodyOG@users.noreply.github.com>
2025-01-26 00:48:50 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -5,15 +5,23 @@ set -e
 # Explicitly use apktool.jar
 wget -q https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.8.1.jar -O apktool.jar  # Download latest version
 
-java -jar apktool.jar d -s iceraven.apk -o iceraven-patched
+# Decompile the APK (without -s option)
+java -jar apktool.jar d iceraven.apk -o iceraven-patched
 
-# Color patching (same as before - VERIFY THESE)
+# Color patching
 sed -i 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">@color\/photonBlack<\/color>/g' iceraven-patched/res/values*/colors.xml
 sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' iceraven-patched/res/values*/colors.xml
 
+# Smali patching
+sed -i 's/ff2b2a33/ff000000/g' iceraven-patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
+sed -i 's/ff42414d/ff15141a/g' iceraven-patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
+sed -i 's/ff52525e/ff15141a/g' iceraven-patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
 
+# Recompile the APK
 java -jar apktool.jar b iceraven-patched -o iceraven-patched.apk --use-aapt2
 
+# Align and sign the APK
 zipalign 4 iceraven-patched.apk iceraven-patched-signed.apk
 
+# Clean up
 rm -rf iceraven-patched iceraven-patched.apk
