246e74672dc0f639c500130f426b4f5036c2f61d
Goodluck Okereke <32042715+GoodyOG@users.noreply.github.com>
2025-01-25 10:07:52 +0000

    build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ set -e
 
 # --- Configuration ---
 APKTOOL_VERSION="2.9.3"
-STATUS_BAR_PADDING="48dp"  # Adjust this value based on testing
+STATUS_BAR_HEIGHT="48dp"  # Tested value for Pixel Fold
 
-# --- Clean previous builds ---
-rm -rf iceraven-patched* *.apk apktool.jar
+# --- Cleanup previous builds ---
+rm -rf iceraven-patched apktool.jar
 
 # --- Download latest Apktool ---
 wget -q "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_${APKTOOL_VERSION}.jar" \
@@ -16,25 +16,21 @@ wget -q "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_${APKTOOL_V
 java -jar apktool.jar d -s iceraven.apk -o iceraven-patched
 
 # --- Apply Dark Theme Mods ---
-echo "Applying dark theme modifications..."
-sed -i \
+find iceraven-patched/res/values* -name 'colors.xml' -exec sed -i \
   -e 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">@color\/photonBlack<\/color>/g' \
   -e 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' \
-  iceraven-patched/res/values*/colors.xml
+  {} +
 
-# --- Fix Pixel Fold Status Bar Padding ---
-echo "Adjusting status bar padding for Pixel devices..."
+# --- Fix Pixel Status Bar Padding ---
 find iceraven-patched/res -name 'dimens.xml' -exec sed -i \
-  -e "s/<dimen name=\"status_bar_height\">[^<]*/<dimen name=\"status_bar_height\">${STATUS_BAR_PADDING}/g" \
-  -e "s/<dimen name=\"status_bar_padding_top\">[^<]*/<dimen name=\"status_bar_padding_top\">${STATUS_BAR_PADDING}/g" \
+  -e "s/<dimen name=\"status_bar_height\">[^<]*/<dimen name=\"status_bar_height\">${STATUS_BAR_HEIGHT}/g" \
+  -e "s/<dimen name=\"status_bar_padding_top\">[^<]*/<dimen name=\"status_bar_padding_top\">${STATUS_BAR_HEIGHT}/g" \
   {} +
 
 # --- Rebuild APK ---
-echo "Rebuilding APK..."
 java -jar apktool.jar b iceraven-patched -o iceraven-patched.apk --use-aapt2
 
-# --- Optimize & Prepare for Signing ---
-echo "Optimizing APK..."
+# --- Optimize APK ---
 zipalign 4 iceraven-patched.apk iceraven-patched-signed.apk
 
 # --- Cleanup ---
