338a4dd3e098c7c3240f50aa324586c340af1b26
Goodluck Okereke <32042715+GoodyOG@users.noreply.github.com>
2025-01-25 10:18:48 +0000

    build.sh

--- build.sh
+++ build.sh
@@ -16,22 +16,37 @@ wget -q "https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_${APKTOOL_V
 java -jar apktool.jar d -s iceraven.apk -o iceraven-patched
 
 # --- Apply Dark Theme Mods ---
+echo "Applying dark theme modifications..."
+
+# XML Color Resources
 find iceraven-patched/res/values* -name 'colors.xml' -exec sed -i \
   -e 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_color_1">@color\/photonBlack<\/color>/g' \
   -e 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' \
   {} +
 
+# Smali Color Values
+echo "Modifying PhotonColors.smali..."
+find iceraven-patched/smali_classes2/mozilla/components/ui/colors -name 'PhotonColors.smali' -exec sed -i \
+  -e 's/ff2b2a33/ff000000/g' \
+  -e 's/ff42414d/ff15141a/g' \
+  -e 's/ff52525e/ff15141a/g' \
+  {} +
+
 # --- Fix Pixel Status Bar Padding ---
+echo "Adjusting status bar dimensions..."
 find iceraven-patched/res -name 'dimens.xml' -exec sed -i \
   -e "s/<dimen name=\"status_bar_height\">[^<]*/<dimen name=\"status_bar_height\">${STATUS_BAR_HEIGHT}/g" \
   -e "s/<dimen name=\"status_bar_padding_top\">[^<]*/<dimen name=\"status_bar_padding_top\">${STATUS_BAR_HEIGHT}/g" \
   {} +
 
 # --- Rebuild APK ---
+echo "Rebuilding APK..."
 java -jar apktool.jar b iceraven-patched -o iceraven-patched.apk --use-aapt2
 
 # --- Optimize APK ---
+echo "Optimizing APK..."
 zipalign 4 iceraven-patched.apk iceraven-patched-signed.apk
 
 # --- Cleanup ---
+echo "Cleaning up..."
 rm -rf iceraven-patched iceraven-patched.apk
